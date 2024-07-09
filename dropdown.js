02c7fa50a0c149537805d489841ea31fb514ff62
nickartemenk <artemenkokit@gmail.com>
2024-07-09 16:47:51 +0300

    change class name

--- dropdown.js
+++ dropdown.js
@@ -1,9 +1,9 @@
 const dropArrow = document.querySelector('.form-service__arrow')
 const service = document.querySelector('.form-service__wrapper')
-const timerMarg =document.querySelector('.form-timer-button')
+const timerBtn =document.querySelector('.form-timer-button')
 
 dropArrow.addEventListener('click', ()=> {
-    service.classList.toggle('form-service-none')
+    service.classList.toggle('hidden')
     dropArrow.classList.toggle('active')
-    timerMarg.classList.toggle('timer-marg')
+    timerBtn.classList.toggle('open-form-service')
 })
