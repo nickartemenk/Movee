32d697408486b30ecce9b96d74eced0cd404ab45
nickartemenk <artemenkokit@gmail.com>
2024-07-09 18:08:46 +0300

    mobile version delete arrow

--- dropdown.js
+++ dropdown.js
@@ -1,8 +1,9 @@
+const dropDown = document.querySelector('.form-service__title-arrow')
 const dropArrow = document.querySelector('.form-service__arrow')
 const service = document.querySelector('.form-service__wrapper')
-const timerBtn =document.querySelector('.form-timer-button')
+const timerBtn = document.querySelector('.form-timer-button')
 
-dropArrow.addEventListener('click', ()=> {
+dropDown.addEventListener('click', ()=> {
     service.classList.toggle('hidden')
     dropArrow.classList.toggle('active')
     timerBtn.classList.toggle('open-form-service')
