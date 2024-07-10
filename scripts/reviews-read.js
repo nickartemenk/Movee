46a53e6b9d6c5262fb01e894195a301c7fd96bbf
nickartemenk <artemenkokit@gmail.com>
2024-07-10 19:30:37 +0300

    delete comments

--- scripts/reviews-read.js
+++ scripts/reviews-read.js
@@ -3,11 +3,10 @@ const readBtns = document.querySelectorAll('.reviews-read');
 readBtns.forEach(readBtn => {
     readBtn.addEventListener('click', () => {
         if (readBtn.textContent === 'Читать полностью') {
-            readBtn.textContent = 'Скрыть'; // Изменяем текст на 'Read Less'
+            readBtn.textContent = 'Скрыть';
         } else {
-            readBtn.textContent = 'Читать полностью'; // Изменяем текст на 'Read More'
+            readBtn.textContent = 'Читать полностью';
         }
-
         readBtn.parentElement.classList.toggle('active')
     });
 });
