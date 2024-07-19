d8b7942b6d2cf4baa0d5f1f909d2ef9aad6f44bf
nickartemenk <artemenkokit@gmail.com>
2024-07-19 18:14:47 +0300

    burger menu fix

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,16 +1,31 @@
-const hamMenu = document.querySelector(".burger-mobile__btn");
-const offScreenMenu = document.querySelector(".menu");
+const burgerMenuBtn = document.querySelector(".burger-mobile__btn");
+const menu = document.querySelector(".menu");
 const btnUp = document.querySelector(".btn-up");
+const menuItems = document.querySelectorAll(".menu-item");
+const body = document.body;
 
-hamMenu.addEventListener("click", () => {
-    hamMenu.classList.toggle("active");
-    offScreenMenu.classList.toggle("active");
+const toggleClasses = () => {
+    burgerMenuBtn.classList.toggle("active");
+    menu.classList.toggle("active");
+    body.classList.toggle("lock");
+};
+
+burgerMenuBtn.addEventListener("click", () => {
+    toggleClasses()
+});
+
+menuItems.forEach(menuItem => {
+    menuItem.addEventListener("click", () => {
+        toggleClasses()
+        btnUp.classList.toggle("active");
+    })
+});
+
+btnUp.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
     btnUp.classList.toggle("active");
-    btnUp.onclick = () => {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }
 });
