81c10b12332eaf9b494a7f6fc8d751a4df039a4c
nickartemenk <artemenkokit@gmail.com>
2024-07-26 16:57:34 +0300

    slider mobile version

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,55 +1,61 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const slider = document.querySelector('.autopark-slider');
-    const track = document.querySelector('.autopark-slider__track');
-    const items = document.querySelectorAll('.autopark-slider__item');
-    const nextButton = document.querySelector('.arrow-right');
-    const prevButton = document.querySelector('.arrow-left');
-    const itemElements = document.querySelectorAll('.autopark-item');
+const track = document.querySelector('.autopark-slider__track');
+const items = document.querySelectorAll('.autopark-slider__item');
+const nextButton = document.querySelector('.arrow-right');
+const prevButton = document.querySelector('.arrow-left');
+const itemElements = document.querySelectorAll('.autopark-item');
+const dots = document.querySelectorAll('.autopark-dots__wrapper');
 
-    let currentIndex = 0;
-    let slideWidth;
+let currentIndex = 0;
 
-    function updateSlideWidth() {
-        slideWidth = window.innerWidth || document.documentElement.clientWidth;
-        track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+function updateSlideWidth() {
+  track.style.transform = `translateX(-${currentIndex * 100}%)`;
 
-        updateArrowVisibility();
-        updateActiveItem();
-    }
+  updateArrowVisibility();
+  updateActiveItem();
+  activeDots();
+}
 
-    function updateArrowVisibility() {
-        prevButton.style.display = currentIndex === 0 ? 'none' : '';
-        nextButton.style.display = currentIndex === items.length - 1 ? 'none' : '';
-    }
+function updateArrowVisibility() {
+  prevButton.style.display = currentIndex === 0 ? 'none' : '';
+  nextButton.style.display = currentIndex === items.length - 1 ? 'none' : '';
+}
 
-    function updateActiveItem() {
-        itemElements.forEach((item, index) => {
-            item.classList.toggle('active', index === currentIndex);
-        });
-    }
+function updateActiveItem() {
+  itemElements.forEach((item, index) => {
+    item.classList.toggle('active', index === currentIndex);
+  });
+}
 
-    window.addEventListener('resize', updateSlideWidth);
-
-    nextButton.addEventListener('click', function () {
-        if (currentIndex < items.length - 1) {
-            currentIndex++;
-            updateSlideWidth();
-        }
-    });
-
-    prevButton.addEventListener('click', function () {
-        if (currentIndex > 0) {
-            currentIndex--;
-            updateSlideWidth();
-        }
-    });
-
-    itemElements.forEach((item, index) => {
-        item.addEventListener('click', function () {
-            currentIndex = index;
-            updateSlideWidth();
-        });
-    });
-
+nextButton.addEventListener('click', function () {
+  if (currentIndex < items.length - 1) {
+    currentIndex++;
     updateSlideWidth();
+  }
+});
+
+prevButton.addEventListener('click', function () {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateSlideWidth();
+  }
+});
+
+itemElements.forEach((item, index) => {
+  item.addEventListener('click', function () {
+    currentIndex = index;
+    updateSlideWidth();
+  });
+});
+
+function activeDots() {
+  dots.forEach((item, index) => {
+    item.classList.toggle('active', index === currentIndex);
+  });
+}
+
+dots.forEach((item, index) => {
+  item.addEventListener('click', function () {
+    currentIndex = index;
+    updateSlideWidth();
+  });
 });
