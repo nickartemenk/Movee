d5aa6c3acf8d703e7143a54b2e52e161046aee48
nickartemenk <artemenkokit@gmail.com>
2024-08-23 16:00:27 +0300

    change html and js

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,14 +1,9 @@
 const timer = () => {
-  const hoursElement = document.querySelector('.form-timer__hours');
-  const minutesElement = document.querySelector('.form-timer__minutes');
-  const secondsElement = document.querySelector('.form-timer__seconds');
+  const timerElement = document.querySelector('.form-timer');
 
-  let totalSeconds = //обшее количество секунд которое должно быть рассчитано
-    parseInt(hoursElement.textContent) * 3600 + //parseInt преобразует текст внутри элемента в числа и умножаем на 3600-коичество секундв часе
-    parseInt(minutesElement.textContent) * 60 +
-    parseInt(secondsElement.textContent);
-
-  // console.log(hoursElement.textContent);
+  let totalSeconds = parseInt(timerElement.textContent.split(':')[0]) * 3600 + // split азбивает строки на массив по разделителю (:) [максимально количество элементов]
+    parseInt(timerElement.textContent.split(':')[1]) * 60 +
+    parseInt(timerElement.textContent.split(':')[2]);
 
   const interval = setInterval(function() { //setInterval вызывает функию регулярно через заданные отрезки времени
     --totalSeconds; //каждую секунду уменьшается на единицу (префиксная и постфиксная форма спросить)
@@ -24,10 +19,8 @@ const timer = () => {
     const minutes = Math.floor((totalSeconds % 3600) / 60); //с помощью взятия остатка от деления на 3600 получаем количество секунд оставшихся после того как из общего количества секунд были вычтены все часы
     const seconds = totalSeconds % 60; //дает количество секунд оставшихся после того как из общего количества секунд были вычтены все минуты и часы
 
-    // обновление текста элементов
-    hoursElement.textContent = String(hours).padStart(2, '0') + ':'; // string преобразует в строку padstart задает минимальное количество символов в строке с нолем слева если это нужно
-    minutesElement.textContent = String(minutes).padStart(2, '0') + ':';
-    secondsElement.textContent = String(seconds).padStart(2, '0');
+    // обновление текста элемента timerElement
+    timerElement.textContent = String(hours).padStart(2, '0') + ':' + String(minutes).padStart(2, '0') + ':' + String(seconds).padStart(2, '0');//string преобразует в строку padstart задает минимальное количество символов в строке с нолем слева если это нужно плюс разделитель в виде :
   }, 1000);
 }
 
